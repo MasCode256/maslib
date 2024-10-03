c4b7c1c60a4e53814c4950375a3a17a88579846d
Never Zero <167694532+MasCode256@users.noreply.github.com>
2024-10-03 19:17:38 +0300

    Add files via upload

--- includes/parser.h
+++ includes/parser.h
@@ -1,1 +1,36 @@
+#ifndef MAS_PARSER_H
+#define MAS_PARSER_H
 
+#include "../mas.h"
+using namespace std;
+
+namespace mas0 {
+    namespace parser {
+        string before(const string& str, char delimiter) {
+            size_t delimiterIndex = str.find(delimiter);
+
+            if (delimiterIndex != std::string::npos) {
+                return str.substr(0, delimiterIndex);
+            } else {
+                return "";
+            }
+
+            return "";
+        }
+
+        string after(const string& str, char delimiter) {
+            size_t pos = str.find(delimiter);
+            if (pos != std::string::npos) {
+                return str.substr(pos + 1);
+            }
+
+            return "";
+        }
+
+        bool contains_char(const string& str, char c) {
+            return str.find(c) != std::string::npos;
+        }
+    }
+}
+
+#endif
